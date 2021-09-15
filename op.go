cda8be692490e409f988fa2d0c006c0e436ecfc9
qiyingwang <qiyingwang@tencent.com>
2021-09-15 11:36:04 +0800

    update

--- op.go
+++ op.go
@@ -1,8 +1,16 @@
 package didagle
 
+type FieldFlags struct {
+	Extern    int `json:"is_extern"`
+	InOut     int `json:"is_in_out"`
+	Agrregate int `json:"is_aggregate"`
+}
+
 type FieldMeta struct {
-	Name string `json:"name"`
-	ID   int64  `json:"id"`
+	Name  string     `json:"name"`
+	ID    int64      `json:"id"`
+	Type  string     `json:"type"`
+	Flags FieldFlags `json:"flags"`
 }
 
 type OperatorMeta struct {
